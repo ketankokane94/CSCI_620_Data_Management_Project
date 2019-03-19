75e35947db775acd2e30b71ecab9e318cc735829
ketankokane94 <ketankokane94@gmail.com>
2019-03-19 19:00:09 -0400

    added to seperate out the title and its genres

--- convert.py
+++ convert.py
@@ -1,10 +1,9 @@
 import pandas as pd 
 
 data = pd.read_csv('title.basics.tsv',sep='\t')
-
-personKnownForTitles = data[['tconst','genres']]
-
-frame = pd.concat([pd.Series(row['tconst'], row['genres'].split(','))              
-                    for _, row in personKnownForTitles.iterrows()])
+temp = data[['tconst','genres']]
+frame = pd.DataFrame(temp.genres.str.split(',').tolist(), index=temp.tconst).stack()
+frame = frame.reset_index()[['tconst',0]]
+frame.columns = ['tconst','genres']
 
 frame.to_csv('title_genres.tsv', sep='\t')
