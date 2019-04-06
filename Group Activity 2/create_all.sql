bf82c57d841a6491b3d1ed3c9fd45bea4e9fe7ab
ketankokane94 <ketankokane94@gmail.com>
2019-04-06 11:38:52 -0400

    garbage collection commit

--- Group Activity 2/create_all.sql
+++
@@ -1,4 +0,0 @@
-create TABLE Aircraft (aid int , aname varchar(300), cruisingrange integer);
-create TABLE Employees (eid int, ename VARCHAR(300) , salary int);
-create TABLE Certified (aid int ,eid int);
-create table Flights (flno int, from_ varchar(500), to_ varchar(500), distance integer, departs time, arrives varchar(100), price varchar(100))
--- convert.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-
-data = pd.read_csv('title.basics.tsv',sep='\t')
-temp = data[['tconst','genres']]
-frame = pd.DataFrame(temp.genres.str.split(',').tolist(), index=temp.tconst).stack()
-frame = frame.reset_index()[['tconst',0]]
-frame.columns = ['tconst','genres']
-
-frame.to_csv('title_genres.tsv', sep='\t')
