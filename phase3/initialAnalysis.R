48e2712b71cdcb3524532165148fa63628c7e185
Siddarth <35558186+ssargunaraj@users.noreply.github.com>
2019-04-07 15:54:32 -0400

    Updated commands and added comments

--- phase3/initialAnalysis.R
+++ phase3/initialAnalysis.R
@@ -1,35 +1,82 @@
+# Introduction to Big Data
+# Phase 3
+# 
+# Data used:
+#     The data is from a census bureau database. 
+#
+# This script file reads the data, cleans it of missing values and visualizes the data by plotting histograms of each feature
+#
+# Dependencies (Libraries used):
+#     1. corrplot (used to display the correlation matrix of the dataset)
+#
 
+#
+# Installing and loading dependencies
+#
 
-
-# the data contains null values
-
+# Install the corrplot library
 install.packages("corrplot")
-
+#Load required libraries
 library(corrplot)
-corrplot(cor(data1[,2:17]), type = "lower")
-corrplot(cor(data1[,2:5]))
 
-data = read.table("Documents/GitHub/CSCI_620_Data_Management_Project/phase3/adult.data", header= TRUE, sep = ",")
-colnames(data)
-str(data)
-summary(data)
-#correlation plot
+#
+# Preprocessing and cleaning the data
+#
+
+# Read the data into a data frame
+adultDataset = read.table("adult.data", header= TRUE, sep = ",")
+# Convert the dataset to integer format
 adultDataset[] <- lapply(adultDataset,as.integer)
+# Removing null values
+na.omit(adultDataset)
+# Attach the database to the R search path
+attach(adultDataset)
+
+#
+# Printing details of the dataset
+#
+
+# Print the summary of the dataset
+summary(adultDataset)
+# Display internal structure of dataset
+str(adultDataset)
+# Print the feature names
+colnames(adultDataset)
+
+#
+# Visualization
+#
+
+# Display the lower correlation plot of the dataset
 corrplot(cor(adultDataset), method="number", type = "lower")
-# plotting histogram 
-plot(data$age)
-plot(data$workclass)
-plot(data$fnlwgt)
-plot(data$education)
-plot(data$education.num)
-plot(data$marital.status)
-plot(data$occupation)
-plot(data$relationship)
-plot(data$race)
-plot(data$sex)
-plot(data$capital.gain)
-plot(data$capital.loss)
-plot(data$hours.per.week)
-plot(data$native.country)
-plot(data$prediction)
+# Display histogram of feature "age" 
+hist(age)
+# Display histogram of feature "workclass"
+hist(workclass)
+# Display histogram of feature "fnlwgt"
+hist(fnlwgt)
+# Display histogram of feature "education"
+hist(education)
+# Display histogram of feature "education.num"
+hist(education.num)
+# Display histogram of feature "marital.status"
+hist(marital.status)
+# Display histogram of feature "occupation"
+hist(occupation)
+# Display histogram of feature "relationship"
+hist(relationship)
+# Display histogram of feature "race"
+hist(race)
+# Display histogram of feature "sex"
+hist(sex)
+# Display histogram of feature "capital.gain"
+hist(capital.gain)
+# Display histogram of feature "capital.loss"
+hist(capital.loss)
+# Display histogram of feature "hours.per.week"
+hist(hours.per.week)
+# Display histogram of feature "native.country"
+hist(native.country)
+# Display histogram of feature "prediction"
+hist(prediction)
 
