ff1c9310f77341f98570ab5f411db4c61a84f566
ketankokane94 <ketankokane94@gmail.com>
2019-04-06 22:23:38 -0400

    Changed the dataset

--- phase3/initialAnalysis.R
+++ phase3/initialAnalysis.R
@@ -1,10 +1,33 @@
-data = read.table("Documents/GitHub/CSCI_620_Data_Management_Project/phase3/letter-recognition.data.txt", sep = ",")
 
-summary(data)
-# the data contains not null values
+
+
+# the data contains null values
 
 install.packages("corrplot")
 
 library(corrplot)
 corrplot(cor(data1[,2:17]), type = "lower")
 corrplot(cor(data1[,2:5]))
+
+data = read.table("Documents/GitHub/CSCI_620_Data_Management_Project/phase3/adult.data", header= TRUE, sep = ",")
+colnames(data)
+str(data)
+summary(data)
+
+# plotting histogram 
+plot(data$age)
+plot(data$workclass)
+plot(data$fnlwgt)
+plot(data$education)
+plot(data$education.num)
+plot(data$marital.status)
+plot(data$occupation)
+plot(data$relationship)
+plot(data$race)
+plot(data$sex)
+plot(data$capital.gain)
+plot(data$capital.loss)
+plot(data$hours.per.week)
+plot(data$native.country)
+plot(data$prediction)
+
