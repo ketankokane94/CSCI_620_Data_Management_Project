ec5cc624fe4567664e1c2e104e6039459abe191f
ketankokane94 <ketankokane94@gmail.com>
2019-04-18 12:36:34 -0400

    added analysis and replaced the missing value with suitable value
    Did not omit the missing value, instead replaced and provided justification for not omiiting

--- phase4/removeMissingValues.R
+++ phase4/removeMissingValues.R
@@ -1,22 +1,42 @@
-
-
 #install.packages("naniar")
 library(naniar)
+#library(dplyr)
 
 # Read the data into a data frame
 dataset = read.table("adult.data",
                      header = TRUE,
                      sep = ",",
                      na.strings = " ?")
-attach(dataset)
+#attach(dataset)
 summary(dataset)
 # total number of rows with NA value
 sum(is.na(dataset))
 
+# find the number of null values for each attribute
 row = sapply(dataset,  function(x)
   sum(is.na(x)))
 
 row = data.frame(row)
-row
-colnames(row)
-plot(row)
+print(row)
+
+# find only those instances where workclass is null
+d1 <- filter(dataset, is.na(workclass))
+summary(d1)
+head(d1)
+# found out that whenever the value of workclass is missing then the value of occupation is also missing,
+# this suggest some co-relation between them.
+
+#replace the NA of WORKCLASS WITH "X".
+dataset$workclass <- as.character(dataset$workclass)
+dataset$workclass[is.na(dataset$workclass)] <- "x"
+dataset$workclass <- factor(dataset$workclass)
+
+dataset$occupation <- as.character(dataset$occupation)
+dataset$occupation[is.na(dataset$occupation)] <- "x"
+dataset$occupation <- factor(dataset$occupation)
+
+d1 <- filter(dataset, is.na(native.country))
+summary(d1)
+
+# when the native.country is NA, the other attribute shows the same trend, assumed the value is missing 
+# because the particular individual was simply unaware of his/her native.country.
