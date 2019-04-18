6bd48c5651308f83e1bfe95de040ed44975de84e
Kavya Kotian <kk2014@rit.edu>
2019-04-18 17:50:25 -0400

    Lowered the number of columns dropped

--- phase4/cleaningDataset.R
+++ phase4/cleaningDataset.R
@@ -1,10 +1,12 @@
 # Read the data into a data frame
 dataset = read.table("adult.data",header = TRUE,sep = ",",na.strings = " ?")
 dim(dataset)
-#attach(dataset)
 
 #Based on the initial analysis, the columns workclass,fnlwgt,race,sex,capital.gain,capital.loss,native.country 
 #are dropped. Values stored in this column are skewed and do not contribute to any useful information
-dataset = subset(dataset, select = -c(workclass,fnlwgt,race,sex,capital.gain,capital.loss,native.country) )
+dataset = subset(dataset, select = -c(fnlwgt,race,native.country) )
 dim(dataset)
 summary(dataset)
+
+#dataset <- na.omit(dataset)
+#dim(dataset)
