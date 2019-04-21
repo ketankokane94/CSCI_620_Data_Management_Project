ccdc5f1581391c867791279cc2052c256d317943
Kavya Kotian <kk2014@rit.edu>
2019-04-21 19:09:19 -0400

    added code to create three models and test on testing and training data both
    remaining to add preparation of testing data code

--- phase4/IncomePredictio.R
+++ phase4/IncomePredictio.R
@@ -2,6 +2,12 @@
 library(naniar)
 library(tidyverse)
 library(GoodmanKruskal)
+library(randomForest)
+library(ggplot2)
+library(rpart)
+library(rpart.plot)
+library(party)
+library(e1071)
 
 # Read the data into a data frame
 dataset = read.table("adult.data",header = TRUE,sep = ",",na.strings = " ?")
@@ -87,7 +93,147 @@ datasetFrame1<- subset(dataset, select = varset1)
 GKmatrix1<- GKtauDataframe(datasetFrame1)
 plot(GKmatrix1, corrColors = "blue")
 
+#Preparing Test data
+
 # tree creation
-library(rpart)
-library(rpart.plot)
+
+
+dtree <- rpart(prediction ~ ., data = dataset, method = 'class', model = TRUE)
+rpart.plot(dtree)
+
+val_predicted <- predict(dtree, dataset, type = "class")
+
+confMatrix <- as.data.frame(table(dataset$prediction, val_predicted))
+names(confMatrix)
+print(confMatrix)
+ggplot(data =  confMatrix, mapping = aes(x = Var1, y = val_predicted)) +
+  ggtitle("Decision Tree Training set confusion matrix")+
+  geom_tile(aes(fill = Freq), colour = "white") +
+  xlab("Actual class label")+
+  ylab("Predicted class label")+
+  geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+  scale_fill_gradient(low = "blue",
+                      high = "red",
+                      trans = "log")+
+  theme(legend.text = element_text( size = 15, hjust = 3, vjust = 3, face = 'bold'))
+# given error
+#accuracy <- sum(diag(confMatrix))/sum(confMatrix)
+
+#print(accuracy)
+
+# run the model on test data
+val_predicted <- predict(dtree, test_data, type = "class")
+
+#print(val_predicted)
+
+confMatrix <- as.data.frame(table(test_data$prediction, val_predicted))
+
+ggplot(data =  confMatrix, mapping = aes(x = Var1, y = val_predicted)) +
+  ggtitle("Decision Tree Testing set confusion matrix")+
+  geom_tile(aes(fill = Freq), colour = "white") +
+  xlab("Actual class label")+
+  ylab("Predicted class label")+
+  geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+  scale_fill_gradient(low = "blue",
+                      high = "red",
+                      trans = "log")
+print(confMatrix)
+
+accuracy <- sum(diag(confMatrix))/sum(confMatrix)
+
+print(accuracy)
+
+
+# Build Naive Bayes Model
+
+model <- naiveBayes(prediction ~ ., data = dataset)
+
+print(model)
+
+# Test model on training data
+
+vals_predicted <- predict(model, newdata = dataset)
+
+confMatrix <- table(dataset$prediction, vals_predicted)
+
+# Prints confusion matrix indicating number of values correctly predicted and not
+
+print(confMatrix)
+
+accuracy <- sum(diag(confMatrix))/sum(confMatrix)
+
+print(accuracy)
+
+# Test model on test data
+
+vals_predicted <- predict(model, newdata = test_data)
+
+confMatrix <- as.data.frame(table(test_data$prediction, vals_predicted))
+confMatrix
+ggplot(data =  confMatrix, mapping = aes(x = Var1, y = vals_predicted)) +
+  ggtitle("Naive Bayes Testing set confusion matrix")+
+  geom_tile(aes(fill = Freq), colour = "white") +
+  xlab("Actual class label")+
+  ylab("Predicted class label")+
+  geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+  scale_fill_gradient(low = "blue",
+                      high = "red",
+                      trans = "log")
+# Prints confusion matrix indicating number of values correctly predicted and not
+
+print(confMatrix)
+
+accuracy <- sum(diag(confMatrix))/sum(confMatrix)
+
+print(accuracy)
+
+
+# random Forest
+dtree <- randomForest(prediction ~ ., data = dataset)
+
+
+val_predicted <- predict(dtree, dataset, type = 'response')
+
+#print(val_predicted)
+
+confMatrix <- as.data.frame(table(dataset$prediction, val_predicted))
+
+print(confMatrix)
+names(confMatrix)
+ggplot(data =  confMatrix, mapping = aes(x = Var1, y = val_predicted)) +
+  geom_tile(aes(fill = Freq), colour = "white") +
+  geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+  scale_fill_gradient(low = "blue",
+                      high = "red",
+                      trans = "log")
+# Plots error rate with respect to increase in number of trees generated
+plot(dtree)
+
+accuracy <- sum(diag(confMatrix))/sum(confMatrix)
+
+print(accuracy)
+
+# On testing data
+levels(test_data$workclass)<-levels(dataset$workclass)
+levels(test_data$occupation)<-levels(dataset$occupation)
+val_predicted <- predict(dtree, test_data, type = 'response')
+
+#print(val_predicted)
+
+confMatrix <- as.data.frame(table(test_data$prediction, val_predicted))
+
+print(confMatrix)
+ggplot(data =  confMatrix, mapping = aes(x = Var1, y = val_predicted)) +
+  ggtitle("Random Forrest Testing set confusion matrix")+
+  geom_tile(aes(fill = Freq), colour = "white") +
+  xlab("Actual class label")+
+  ylab("Predicted class label")+
+  geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+  scale_fill_gradient(low = "blue",
+                      high = "red",
+                      trans = "log")
+# Plots error rate with respect to increase in number of trees generated
+plot(dtree)
+
+accuracy <- sum(diag(confMatrix))/sum(confMatrix)
 
